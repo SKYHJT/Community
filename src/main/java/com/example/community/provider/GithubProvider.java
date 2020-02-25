7c384c2ee6491f072bb46debf76064d1f5404b24
SKYHJT <421947349@qq.com>
2020-02-25 10:00:02 +0800

    extract github from code to proterties

---
+++ src/main/java/com/example/community/provider/GithubProvider.java
@@ -0,0 +1,54 @@
+package com.example.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GithunUserDTO;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @ClassName GithubProvider
+ * @Description TODO
+ * @Author hujingtao
+ * @Date 2020/2/24 11:21
+ * @Version 1.0
+ **/
+@Component // Component注解可以将对象自动添加到spring容器中，无需手动实例化，spring会帮助完成
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            System.out.println(token);
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithunUserDTO getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithunUserDTO githubUser = JSON.parseObject(string, GithunUserDTO.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
